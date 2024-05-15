b7e4d84cb381d201ed64383d5d1ec96c8b3643fd
Jazmin-Moreno <jazmin.moreno01@uptc.edu.co>
2024-05-14 21:45:38 -0500

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import Singin from './pages/Signin'
 import SingUp from './pages/SingUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path= "/" element={<Home />}></Route>
         <Route path= "/about" element={<About />}></Route>
